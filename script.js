f13bb35479a62067da34517c67927327599b7414
tidanti <tipanovdaniil@gmail.com>
2022-01-25 23:12:57 +0300

    Added index.html and script.js; added some primary functions in script file

---
+++ script.js
@@ -0,0 +1,62 @@
+// input values
+
+// check values
+
+// show error (msg)
+
+// computer plays - DONE!
+
+// get round result - DONE!
+
+// show result
+
+const ROCK = 'Rock';
+const PAPER = 'Paper';
+const SCISSORS = 'Scissors';
+const DRAW = 'Draw';
+
+function showErrorMessage(msg) {
+    console.log(msg);
+}
+
+function checkPlayerInput(inputValue) {
+    
+}
+
+function calcPlayerMove(inputValue) {
+
+}
+
+function computerPlay() {
+    let computerMove = Math.floor(Math.random() * 3); // num from 0 to 2
+    switch (computerMove) {
+        case 0:
+            computerMove = ROCK;
+            break;
+        case 1:
+            computerMove = PAPER;
+            break;
+        case 2:
+            computerMove = SCISSORS;
+            break;
+        default:
+            computerMove = null;
+            //showErrorMessage('Something has gone wrong in computerPlay!');
+    }
+
+    return computerMove;
+}
+
+function playRound(playerMove, computerMove) {
+    if ((playerMove === ROCK && computerMove === SCISSORS) ||
+        playerMove === PAPER && computerMove === ROCK ||
+        playerMove === SCISSORS && computerMove === PAPER) {
+            return playerMove;
+    
+    } else if (playerMove === computerMove) {
+        return DRAW;
+    } else {
+        return computerMove;
+    }
+}
+
